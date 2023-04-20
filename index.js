56fb267a39ed0649a43f5dd4166e3630eca97c02
frckbrice <maebrie2017@gmail.com>
2023-04-20 11:19:13 +0100

    strike the all the line but not only the text

--- index.js
+++ index.js
@@ -2,22 +2,19 @@
 
 const submit = document.querySelector(".add-task-btn");
 const listOfTasks = document.querySelector(".list-of-tasks");
-const searchFilter = document.querySelector('.search-input');
-const strikeText = document.querySelectorAll('.strike')
-
+const searchFilter = document.querySelector(".search-input");
+const strikeText = document.querySelectorAll(".strike");
 
 // add events
 submit.addEventListener("click", addTask);
 listOfTasks.addEventListener("click", removeBtn);
 searchFilter.addEventListener("keyup", searchFilterFn);
 Array.from(strikeText).forEach((item) => {
-  item.addEventListener('click',strikeTextFn);
-})
-
+  item.addEventListener("click", strikeTextFn);
+});
 
 /* function : add task */
-function addTask(){
-
+function addTask() {
   // get input text from user
   const inputText = document.querySelector(".input-text").value;
 
@@ -28,13 +25,13 @@ function addTask(){
 
   //create delete button1
   const deleteButton = document.createElement("button");
-  deleteButton.style.cursor = 'pointer'
+  deleteButton.style.cursor = "pointer";
   deleteButton.appendChild(document.createTextNode("X"));
   deleteButton.className = "delete-btn btn";
 
   //create delete button2
   const deleteButton2 = document.createElement("button");
-  deleteButton2.style.cursor = 'pointer'
+  deleteButton2.style.cursor = "pointer";
   deleteButton2.appendChild(document.createTextNode(""));
   deleteButton2.className = "delete-btn";
 
@@ -47,7 +44,7 @@ function addTask(){
   deleteButton2.appendChild(iIcon);
 
   //create container for buttons
-  const divIcons = document.createElement('div');
+  const divIcons = document.createElement("div");
   divIcons.className = "icons";
 
   //add the two buttons to container
@@ -61,9 +58,8 @@ function addTask(){
   listOfTasks.appendChild(li);
 }
 
-
 /* function :  remove task */
-function removeBtn(e){
+function removeBtn(e) {
   // to target the true element to delete
   const deleteBtn = e.target;
   if (deleteBtn.classList.contains("delete")) {
@@ -77,75 +73,40 @@ function removeBtn(e){
 }
 
 // function to strike text from a task
-function strikeTextFn(e){
-  console.log('within the strike fnt')
+function strikeTextFn(e) {
+  console.log("within the strike fnt");
   const li = e.target.parentElement.parentElement.parentElement;
-  console.log(li)
-  console.log(li.firstChild.textContent)
-  const textToStrike = li.firstChild.textContent
-  if(confirm('Are you done with this task ?')) {
-    
-    li.firstChild.innerHmlt = textToStrike.strike()
-    console.log(textToStrike)
+  console.log(li);
+  console.log(li.firstChild.textContent);
+  const textToStrike = li.firstChild.textContent;
+  if (confirm("Are you done with this task ?")) {
+    li.firstChild.innerHTML = textToStrike.strike();
+    // li.innerHTML = textToStrike.strike();
+    // console.log(textToStrike);
     // li.setAttribute('text-decoration', 'line-through');
-  //textToStrike= `<del>${textToStrike}</del>`
-}
+    //textToStrike= `<del>${textToStrike}</del>`
+  }
 }
 
 /* function : seach for a task in in the list of tasks */
-function searchFilterFn(e){
+function searchFilterFn(e) {
   //target the filter element
   // const searchFilter = document.querySelector('.search-input')
   // to convert input text to lowercase
   let textForSearch = e.target.value.toLowerCase();
-  // target all the tasks to be remove 
-  const tasksToRemove = document.getElementsByTagName('li');
+  // target all the tasks to be remove
+  const tasksToRemove = document.getElementsByTagName("li");
   // conert the result in array
   Array.from(tasksToRemove).forEach((task) => {
     // to get the content of all the tasks
     let taskItemChild = task.firstChild.textContent;
     // verify if the search text exist in the task content
-    if(taskItemChild.toLocaleLowerCase().indexOf(textForSearch)!=-1){
+    if (taskItemChild.toLocaleLowerCase().indexOf(textForSearch) != -1) {
       // if existence display
-      task.style.display = 'block';
-    }else {
+      task.style.display = "block";
+    } else {
       // else hide
-      task.style.display = 'none';
+      task.style.display = "none";
     }
-  })
+  });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
